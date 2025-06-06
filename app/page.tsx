d9a29580cfe994b8e10b6104ace26cd855d9cfed
Rishabh <rishabh19772004@gmail.com>
2025-06-06 15:20:57 +0530

    done with homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard'
+import CTA from '@/components/CTA'
+import CompanionsList from '@/components/CompanionList'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <>
-      <h1 className='font-bold bg-blend-color-dodge'>hel0ooooo</h1>
-      <Button>Click me</Button>
-    </>
+    <main>
+      <h1 className=''>Popular Companions</h1>
+      <section className='home-section'>
+         <CompanionCard id="1" name="John Doe" topic="Math" subject="Algebra" duration={10} color="red" />
+         <CompanionCard id="2" name="Jane Doe" topic="Math" subject="Algebra" duration={10} color="blue" />
+         <CompanionCard id="3" name="John Doe" topic="Math" subject="Algebra" duration={10} color="green" />
+      </section>
+      <section className='home-section'>
+        <CompanionsList 
+        title="Recently completed lessons"
+        companions={recentSessions}
+        classNames="w-2/3 max"  
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
